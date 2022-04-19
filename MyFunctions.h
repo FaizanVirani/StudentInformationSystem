f322e895b80e80701d6b37098899132e23c65658
FaizanVirani <104014327+FaizanVirani@users.noreply.github.com>
2022-04-19 10:33:24 -0400

    Create MyFunctions.h

---
+++ MyFunctions.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+vector<string> splitString(string, char);
+void worksheet1(string);
+void getInformation();
+void viewInformation();
+bool digit(string);
+bool accounts();
+int getCredits();
+int getGrade();
+double getAvg(vector<string>, int, int);
